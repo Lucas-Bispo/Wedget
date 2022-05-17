6934270c91dfb8afe4322e4951fb91fc854c609d
Lucas-Bispo <lukaobispo@gmail.com>
2022-05-17 10:34:04 -0300

    :truck: Rotas

---
+++ server/src/routes.ts
@@ -0,0 +1,53 @@
+import express from 'express';
+import nodemailer from 'nodemailer'
+
+import { prisma } from './prisma';
+
+export const routes = express.Router()
+
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "59c6e3fce81aa7",
+      pass: "d1e0759c0313a1"
+    }
+});
+
+routes.post('/feedbacks', async (req, res) => {
+
+    const {type, comment, screenshot} = req.body;
+
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot,
+        }
+    })
+
+    transport.sendMail({
+        from: 'Equipe Feedget <oi@feedback.com>',
+        to: 'Lucas Bispo <lukaobispo@gmail.com>',
+        subject: 'Novo feedback',
+        html: [
+            `<div style="font-fammily: sans-serif; font-size: 16px; color: #111">`,
+            `<p> Tipo do Feedback: ${type}</p>`,
+            `<p> Tipo do Feedback: ${comment}</p>`,
+            `</div>`
+        ].join('\n')
+    
+    });
+
+   
+    
+    return res.status(201).json(feedback);
+})
+
+
+
+
+
+
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express'
-import nodemailer from 'nodemailer'
 
-import { prisma } from './prisma';
+import { routes } from './routes';
 
 const app = express();
 
@@ -9,46 +8,10 @@ app.use(express.json());
 
 
 
-const transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "59c6e3fce81aa7",
-      pass: "d1e0759c0313a1"
-    }
-});
 
 
+app.use(routes)
 
-
-app.post('/feedbacks', async (req, res) => {
-
-    const {type, comment, screenshot} = req.body;
-
-
-    const feedback = await prisma.feedback.create({
-        data: {
-            type,
-            comment,
-            screenshot,
-        }
-    })
-
-    transport.sendMail({
-        from: 'Equipe Feedget <oi@feedback.com>',
-        to: 'Lucas Bispo <lukaobispo@gmail.com>',
-        subject: 'Novo feedback',
-        html: [
-            `<div style="font-fammily: sans-serif; font-size: 16px; color: #111">`,
-            `<p> Tipo do Feedback: ${type}</p>`,
-            `<p> Tipo do Feedback: ${comment}</p>`,
-            `</div>`
-        ].join('\n')
-
-    });
-    
-    return res.status(201).json(feedback);
-})
 
 
 app.listen(3333,()=>{
