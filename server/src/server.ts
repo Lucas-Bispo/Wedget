bddec1ad950f4a8415435e7e4243eb7a7a85eb6d
Lucas-Bispo <lukaobispo@gmail.com>
2022-05-17 10:15:19 -0300

    :arrow_up: Akustes para testes de API

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,53 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
+
+import { prisma } from './prisma';
 
 const app = express();
 
-app.get('/users', (rep, res) => {
-    return res.send('Hello World');
+app.use(express.json());
+
+
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "59c6e3fce81aa7",
+      pass: "d1e0759c0313a1"
+    }
+});
+
+
+
+
+app.post('/feedbacks', async (req, res) => {
+
+    const {type, comment, screenshot} = req.body;
+
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot,
+        }
+    })
+
+    transport.sendMail({
+        from: 'Equipe Feedget <oi@feedback.com>',
+        to: 'Lucas Bispo <lukaobispo@gmail.com>',
+        subject: 'Novo feedback',
+        html: [
+            `<div style="font-fammily: sans-serif; font-size: 16px; color: #111">`,
+            `<p> Tipo do Feedback: ${type}</p>`,
+            `<p> Tipo do Feedback: ${comment}</p>`,
+            `</div>`
+        ].join('\n')
+
+    });
+    
+    return res.status(201).json(feedback);
 })
 
 
