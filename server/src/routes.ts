32512916c769e202b1da315b2569714de1b390e2
Lucas-Bispo <lukaobispo@gmail.com>
2022-05-17 11:47:05 -0300

    :memo: Sintaxe

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import nodemailer from 'nodemailer'
 
+
 import { prisma } from './prisma';
+
+import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository'
+import { SubmitFeedbackUseCase} from './use-cases/submit-feedback-use-case'
 
 export const routes = express.Router()
 
@@ -19,25 +23,33 @@ routes.post('/feedbacks', async (req, res) => {
 
     const {type, comment, screenshot} = req.body;
 
+    const PrismaFeedbackRepository = new PrismaFeedbacksRepository()
+    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+        PrismaFeedbackRepository
+    )
 
-    const feedback = 
-
-    transport.sendMail({
-        from: 'Equipe Feedget <oi@feedback.com>',
-        to: 'Lucas Bispo <lukaobispo@gmail.com>',
-        subject: 'Novo feedback',
-        html: [
-            `<div style="font-fammily: sans-serif; font-size: 16px; color: #111">`,
-            `<p> Tipo do Feedback: ${type}</p>`,
-            `<p> Tipo do Feedback: ${comment}</p>`,
-            `</div>`
-        ].join('\n')
-    
+    await submitFeedbackUseCase.execute({
+        type,
+        comment,
+        screenshot,
     });
+
+  // await transport.sendMail({
+    //    from: 'Equipe Feedget <oi@feedback.com>',
+      //  to: 'Lucas Bispo <lukaobispo@gmail.com>',
+        //subject: 'Novo feedback',
+       // html: [
+        //    `<div style="font-fammily: sans-serif; font-size: 16px; color: #111">`,
+          //  `<p> Tipo do Feedback: ${type}</p>`,
+            //`<p> Tipo do Feedback: ${comment}</p>`,
+            //`</div>`
+   //     ].join('\n')
+    
+    //});
 
    
     
-    return res.status(201).json(feedback);
+    return res.status(201).send();
 })
 
 
