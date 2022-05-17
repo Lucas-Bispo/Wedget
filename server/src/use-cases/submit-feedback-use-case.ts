4bc1fd6cd0c2f2c48c10523f2aaa205e8aa54ff0
Lucas-Bispo <lukaobispo@gmail.com>
2022-05-17 14:55:44 -0300

    :test_tube: Teste finalizado

--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -18,6 +18,18 @@ export class SubmitFeedbackUseCase{
     async execute(request:SubmitFeedbackUseCaseRequest){
         const {type, comment, screenshot} = request;
 
+        if(!type){
+            throw new Error('Type is required');
+        }
+        if(!comment){
+            throw new Error('Type is required');
+        }
+
+
+        if(screenshot && !screenshot.startsWith('data:image/png;base64')){
+            throw new Error('Ivalid screenshot format.')
+        }
+
 
         await this.feedbackRepository.create({
             type,
