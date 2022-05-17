b278814772d05121d13af62b5285310fabbe1f42
Lucas-Bispo <lukaobispo@gmail.com>
2022-05-17 14:42:32 -0300

    :test_tube: Teste bem sucedido

--- server/src/use-cases/submit-feedback-use-case.spec.ts
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,3 +1,16 @@
-test('sum 2 + 2', () => {
-    expect( 2 + 2).toBe(4)
+import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
+
+describe('Submit feedback', () => {
+    it('should be able to submit feedback', async () => {
+        const submitFeedback = new SubmitFeedbackUseCase(
+            { create: async () => {}},
+            {sendMail: async () => {}}
+        )
+
+       await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: 'Example comment',
+            screenshot: 'test.jpg',
+        })).resolves.not.toThrow();
+    });
 });
