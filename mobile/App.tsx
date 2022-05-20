153acfbfe81aeac839c51487a1df548e6438e2a2
Lucas-Bispo <lukaobispo@gmail.com>
2022-05-20 18:01:24 -0300

    :lipstick: Estilização de botão

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,35 +1,47 @@
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar} from 'expo-status-bar';
+
+
 import { View } from 'react-native';
-import AppLoading from 'expo-app-loading';
-import {
-  useFonts,
-  Inter_400Regular,
-  Inter_500Medium,
-} from '@expo-google-fonts/inter';
+import  AppLoading  from 'expo-app-loading';
+import {useFonts, Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter';
 
-import { theme } from './src/theme';
-import { Widget } from './src/components/Widget';
 
-export default function App() {
+
+import {theme} from './src/theme';
+import Widget from './src/components/Widget';
+import React from 'react';
+
+export default function App(){
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
-    Inter_500Medium,
+    Inter_500Medium
   });
 
-  if (!fontsLoaded) {
+  if(!fontsLoaded){
     return <AppLoading />;
   }
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: theme.colors.background,
-      }}
+    <View style={{
+      flex:1,
+      backgroundColor: theme.colors.background,
+    }}
     >
-      <StatusBar style="light" backgroundColor="transparent" translucent />
-      <Widget />
-    </View>
+      <StatusBar
+      style="light"
+      backgroundColor="transparent"
+      translucent
+      />
+
+      <Widget  />
+      
+      </View>
   );
+
+
+
+
+
+  
 }
