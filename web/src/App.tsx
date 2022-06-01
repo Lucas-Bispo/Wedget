98ec730c9adc44e9d5e32132b18156a44566ec3e
Lucas-Bispo <lukaobispo@gmail.com>
2022-06-01 09:30:59 -0300

    :rocket: Tes de Deploy

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,16 @@
+import { GridCards } from "./components/GridCards";
+import { Navbar } from "./components/Navbar";
 import { WidgetButton } from "./components/WidgetButton";
 
-export const App = () => {
-  return <WidgetButton />;
-};
+
+export function App() {
+  return (
+    <div className="bg-zinc-50 dark:bg-[#09090A] text-zinc-300 transition-colors duration-500 min-h-[100vh] pb-8">
+      <Navbar />
+      <GridCards />
+      <div className="fixed right-0 bottom-3 z-2">
+        <WidgetButton />
+      </div>
+    </div>
+  );
+}
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,17 +1,31 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
         brand: {
-          300: "#996dff",
-          500: "#8257e6",
+          300: "#996DFF",
+          500: "#8257e6"
         },
+        darkMode: {
+          bg: "#27272A",
+          component: "#3F3F46",
+          hover: "#18181B"
+        },
+        lightMode: {
+          bg: "#F4F4F5",
+          component: "#F4F4F5",
+          text: "#27272A",
+        }
       },
       borderRadius: {
-        md: "4px",
-      },
+        md: "4px"
+      }
     },
   },
-  plugins: [require("@tailwindcss/forms"), require("tailwind-scrollbar")],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 };
